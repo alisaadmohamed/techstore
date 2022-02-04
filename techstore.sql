871f3999fdaa28f79cd70e74dffa4ed0c2baaef5
aliss <57112299+alisaadmohamed@users.noreply.github.com>
2022-02-04 17:48:52 +0100

    Add files via upload

--- techstore.sql
+++ techstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 19, 2022 at 05:20 PM
+-- Generation Time: Feb 04, 2022 at 04:47 PM
 -- Server version: 10.5.13-MariaDB
 -- PHP Version: 7.4.27
 
@@ -41,7 +41,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `email`, `password`, `is_super`, `created_at`) VALUES
-(1, 'ali saad', 'ali_saad2003@hotmail.com', '$2y$10$0EJ.1W8rahQkF25bHVZCCuqj9UCV5jyNUldzWTLpmMuYE4eIWEpGW', 'no', '2022-01-06 01:37:46');
+(1, 'malik', 'ali_saad2003@hotmail.com', '$2y$10$Rkx6MRcL9uVNbTFA/YQIQ.UcaAYZZ2/wZ35L8mln/HRnMcQGgU5n6', 'no', '2022-01-06 01:37:46');
 
 -- --------------------------------------------------------
 
@@ -86,7 +86,19 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `name`, `email`, `phone`, `address`, `status`, `created_at`) VALUES
-(2, 'ali', 'ali@hot.com', '002012', NULL, 'pending', '2022-01-07 19:25:22');
+(18, 'Mahmoud Ali', 'ali.isra96@outlook.com', '+436766807260', 'Kajetan-sweth-straÃŸe54', 'approved', '2022-01-20 21:58:52'),
+(19, 'Mahmoud Ali', NULL, '+436766807260', 'AnzengruberstraÃŸe\r\n1/20', 'approved', '2022-01-20 22:03:10'),
+(20, 'aya', 'a@hot.com', '012', NULL, 'pending', '2022-01-20 22:04:50'),
+(21, 'Israa Ali', 'ali.isra96@outlook.com', '+436766807260', 'AnzengruberstraÃŸe\r\n1/20', 'pending', '2022-01-21 12:36:08'),
+(22, 'Israa Ali', 'ali.isra96@outlook.com', '+436766807260', 'AnzengruberstraÃŸe\r\n1/20', 'pending', '2022-01-22 13:39:22'),
+(23, 'Israa Ali', 'ali.isra96@outlook.com', '+436766807260', 'AnzengruberstraÃŸe\r\n1/20', 'pending', '2022-01-22 18:25:52'),
+(24, 'ali', 'ali@hot.com', '555', 'kj', 'pending', '2022-01-28 21:57:21'),
+(25, 'malik', 'a@a.com', '58555', 'gfhjm,', 'pending', '2022-01-28 22:00:37'),
+(26, 'Ali Abdelhamid', 'ali_saad2003@hotmail.com', '06605195599', 'Anzengruberstr.1/20', 'approved', '2022-01-28 22:03:47'),
+(27, 'anything', 'a@ahot.com', '00433', 'there', 'pending', '2022-02-04 14:44:25'),
+(28, 'noor', 'noor@hot.com', '555', 'dkldkdkdkdk', 'approved', '2022-02-04 14:46:57'),
+(29, 'aya', 'aya@hot.com', '00121', 'home', 'canceled', '2022-02-04 16:17:00'),
+(30, 'iss Ali', 'ali.isra96@outlook.com', '+436766807260', 'AnzengruberstraÃŸe\r\n1/20', 'pending', '2022-02-04 16:40:18');
 
 -- --------------------------------------------------------
 
@@ -100,6 +112,28 @@ CREATE TABLE `orders_details` (
   `products_id` int(10) UNSIGNED DEFAULT NULL,
   `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `orders_details`
+--
+
+INSERT INTO `orders_details` (`id`, `order_id`, `products_id`, `qty`) VALUES
+(8, 18, 44, 1),
+(10, 19, 42, 1),
+(11, 20, NULL, 1),
+(12, 21, NULL, 1),
+(13, 22, NULL, 1),
+(14, 23, NULL, 1),
+(15, 24, NULL, 1),
+(16, 25, NULL, 1),
+(17, 26, NULL, 1),
+(18, 26, 44, 1),
+(19, 28, 42, 1),
+(20, 28, 44, 1),
+(21, 28, 45, 4),
+(22, 29, 45, 4),
+(23, 29, 42, 3),
+(24, 30, 44, 1);
 
 -- --------------------------------------------------------
 
@@ -123,15 +157,11 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `desc`, `price`, `quantity`, `img`, `cat_id`, `created_at`) VALUES
-(1, 'lenovo ipad', 'this is dummy description for product', 1500, 10, '1.jpg', 10, '2022-01-10 22:50:40'),
-(2, 'pc', 'this is dummy description for product', 1500, 10, '2.jpg', 5, '2022-01-10 22:52:25'),
-(40, 'apple iphone', 'this is dummy description for product', 3000, 20, '4.jpg', 6, '2022-01-10 22:50:40'),
 (42, 'apple laptop', 'this is dummy description for product', 3000, 20, '4.jpg', 5, '2022-01-10 22:52:25'),
 (44, 'smart phone', 'this is dummy description for product', 150, 33, '5.jpg', 6, '2022-01-10 22:53:45'),
-(45, 'HP pc', 'this is dummy description for product', 25000, 10, '2.jpg', 5, '2022-01-10 22:58:34'),
-(46, 'Sony pc', 'this is dummy description for product', 4000, 3, '2.jpg', 5, '2022-01-10 22:58:34'),
-(47, 'HP Laptop', 'Dummy Data', 2000, 5, '2.jpg', 4, '2022-01-13 22:06:21'),
-(48, 'Aser', 'Dummy data', 1800, 2, '2.jpg', 4, '2022-01-13 22:06:21');
+(45, 'HP pc', 'this is dummy description for product  ', 25000, 10, '2.jpg', 6, '2022-01-10 22:58:34'),
+(55, 'Ali', '7uio      ', 255, 55, '61f7f76aeb203.jpg', 4, '2022-01-31 14:47:04'),
+(56, 'apple ipad', 'ipad', 5580, 6, '2.jpg', 10, '2022-02-04 16:36:38');
 
 --
 -- Indexes for dumped tables
@@ -141,7 +171,8 @@ INSERT INTO `products` (`id`, `name`, `desc`, `price`, `quantity`, `img`, `cat_i
 -- Indexes for table `admins`
 --
 ALTER TABLE `admins`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `cats`
@@ -190,19 +221,19 @@ ALTER TABLE `cats`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `orders_details`
 --
 ALTER TABLE `orders_details`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- Constraints for dumped tables
